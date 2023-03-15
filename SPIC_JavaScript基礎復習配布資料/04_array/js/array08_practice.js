1167989879480e9e5db26acaf8cbf0872f0b2f8d
akihirofukumura <104131582+akihirofukumura@users.noreply.github.com>
2023-03-16 00:05:45 +0900

    finish [array08.practice]
    add:
     "中吉" is not  sentence.
    
    I added sentence it.

--- SPIC_JavaScript基礎復習配布資料/04_array/js/array08_practice.js
+++ SPIC_JavaScript基礎復習配布資料/04_array/js/array08_practice.js
@@ -1,2 +1,19 @@
 // 画面を更新する度に0-5までのランダムな値が入る
-const no = Math.floor(Math.random()*5);
+const no = Math.floor(Math.random() * 5);
+
+let omikuji = ["大吉", "中吉", "小吉", "吉", "凶", "大凶"];
+console.log(omikuji);
+
+if (no == 0) {
+  console.log(omikuji[0] + "ですおめでとう！");
+} else if (no == 1) {
+  console.log(omikuji[1] + "ですラッキー！");
+} else if (no == 2) {
+  console.log(omikuji[2] + "です少しラッキー！");
+} else if (no == 3) {
+  console.log(omikuji[3] + "です");
+} else if (no == 4) {
+  console.log(omikuji[4] + "ですアンラッキー");
+} else if (no == 5) {
+  console.log(omikuji[5] + "です残念・・・");
+}
