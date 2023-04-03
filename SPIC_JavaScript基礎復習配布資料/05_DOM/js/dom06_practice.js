658b201bdea9de4d500f9c43a056a163e701b011
akihirofukumura <104131582+akihirofukumura@users.noreply.github.com>
2023-04-04 04:22:02 +0900

    finish [dom06_practice.js]

--- SPIC_JavaScript基礎復習配布資料/05_DOM/js/dom06_practice.js
+++ SPIC_JavaScript基礎復習配布資料/05_DOM/js/dom06_practice.js
@@ -2,3 +2,9 @@ const main = document.getElementById("main");
 const sub = document.getElementById("sub");
 const soup = document.getElementById("soup");
 const side = document.getElementById("side");
+const btn = document.getElementById("submit");
+const output = document.getElementById("menu");
+
+btn.addEventListener("click", () => {
+  output.innerText = `注文料理は${main.value},${sub.value},${soup.value},${side.value}です。`;
+});
