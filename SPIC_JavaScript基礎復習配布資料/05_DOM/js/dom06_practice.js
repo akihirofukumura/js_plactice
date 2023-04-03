c2bbff6793bfd3abe22477101719c9962701c958
akihirofukumura <104131582+akihirofukumura@users.noreply.github.com>
2023-04-03 17:11:23 +0900

    input id [dom06_practice.js]

---
+++ SPIC_JavaScript基礎復習配布資料/05_DOM/js/dom06_practice.js
@@ -0,0 +1,4 @@
+const main = document.getElementById("main");
+const sub = document.getElementById("sub");
+const soup = document.getElementById("soup");
+const side = document.getElementById("side");
