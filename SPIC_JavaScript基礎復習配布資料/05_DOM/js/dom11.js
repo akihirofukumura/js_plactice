552ff6a7394a09987d1f4d0e5fa99fcbb0e33b83
akihirofukumura <104131582+akihirofukumura@users.noreply.github.com>
2023-04-24 19:24:49 +0900

    finish [dom11.js]

--- SPIC_JavaScript基礎復習配布資料/05_DOM/js/dom11.js
+++ SPIC_JavaScript基礎復習配布資料/05_DOM/js/dom11.js
@@ -1,1 +1,8 @@
 const btn = document.getElementsByClassName("changeBtn");
+const bg = document.getElementById("bg");
+
+for (let i = 0; i < btn.length; i++) {
+  btn[i].addEventListener("click", (evt) => {
+    bg.style.backgroundColor = btn[i].style.backgroundColor;
+  });
+}
