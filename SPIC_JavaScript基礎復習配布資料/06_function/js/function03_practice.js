ed7708f8851e96d16d789bce158733b104a5d3ec
akihirofukumura <104131582+akihirofukumura@users.noreply.github.com>
2023-04-27 17:58:49 +0900

    blank

--- SPIC_JavaScript基礎復習配布資料/06_function/js/function03_practice.js
+++ SPIC_JavaScript基礎復習配布資料/06_function/js/function03_practice.js
@@ -2,15 +2,6 @@ const msg = document.getElementById("text");
 const btn = document.getElementById("btn");
 const out = document.getElementById("out");
 
-// function message(val) {
-//   out.innerText = `あなたが出力した文字は${msg.value}です`;
-// }
-
-// btn.onclick = () => {
-//   message();
-// };
-
-// 引数使用
 function message(val) {
   out.innerText = `あなたが出力した文字は${msg.value}です`;
 }
